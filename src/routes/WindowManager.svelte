6670a73a6ad0a2113f728e0c18a2b4b4f486bbbb
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-10-03 22:59:18 +0200

    Draggable change

--- src/routes/WindowManager.svelte
+++ src/routes/WindowManager.svelte
@@ -5,42 +5,126 @@
     function closeWindow(id) {
       windows.update(n => n.filter(win => win.id !== id));
     }
-  </script>
+</script>
   
   {#each $windows as {id, content}}
-    <Draggable>
-      <div class="window">
-        <div class="title-bar">
-          <button on:click={() => closeWindow(id)}>X</button>
-          <span>{content.title}</span>
-        </div>
+    <div class="irix-border">
+      <div class="irix-inner">
+        <Draggable>
+          <div class="title-bar">
+            <button on:click={() => closeWindow(id)}>X</button>
+            <span>{content.title}</span>
+          </div>
+        </Draggable>
         <div class="window-content">
           {content.body}
         </div>
       </div>
-    </Draggable>
+    </div>
   {/each}
   
-  <style>
-    .window {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        border: 3px solid var(--color-theme-1);
-        background-color: var(--color-theme-4);
-        box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
-    }
-    .title-bar {
-      background-color: var(--color-theme-3);
-      padding: 5px;
-      font-size: 14px;
-      font-weight: bold;
-      font-style: italic;
-      color: white;
-      cursor: grab;
-    }
-    .window-content {
-      padding: 10px;
-    }
-  </style>
+<style>
+  .title-bar {
+    position: relative;
+    background-color: var(--color-theme-3);
+    padding: 4px;
+    font-size: 14px;
+    font-weight: bold;
+    font-style: italic;
+    color: white;
+    z-index: 2;
+    cursor: grab;
+    border-bottom: 1px solid darkslategray;
+  }
+
+  .title-bar:before {
+    border-bottom: 2px solid black;
+  }
+
+  .title-bar:after {
+    border-bottom: 1px solid #999;
+    
+  }
+
+  .title-bar:before, .title-bar:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  .window-content {
+    padding: 10px;
+    border-top: 1px solid black;
+    z-index: 2;
+  }
+
+
+  .irix-border {
+    position: relative;
+    width: 300px;
+    height: 200px;
+    background-color: var(--color-theme-4); /* Adjust as per your needs */
+    border: 1px solid black; /* Base border color */
+  }
   
+  .irix-border:before, .irix-border:after, .irix-inner:before, .irix-inner:after {
+    content: "";
+    position: absolute;
+    box-sizing: border-box;
+  }
+  
+  /* Outer top and left light border */
+  .irix-border:before {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-top: 1px solid #999;
+    border-left: 1px solid #999;
+    z-index: 1;
+  }
+  
+  /* Inner bottom and right dark border */
+  .irix-inner:after {
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    top: 0px;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+    z-index: 1;
+  }
+  
+  /* Outer bottom and right slightly-lighter border */
+  .irix-border:after {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-right: 1px solid #999;
+    border-bottom: 1px solid #999;
+    z-index: 1;
+  }
+  
+  /* Inner top and left slightly-darker border */
+  .irix-inner:before {
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    top: 0px;
+    border-left: 1px solid black;
+    border-top: 1px solid black;
+    z-index: 1;
+  }
+  
+  .irix-inner {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    border: 2px solid darkslategray;
+  }
+</style>
