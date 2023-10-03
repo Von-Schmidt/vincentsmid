0696254c8c95adde19eecedb83dd6f8c182a86ab
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-10-03 22:59:29 +0200

    TaskBar styling #1

--- src/routes/TaskBar.svelte
+++ src/routes/TaskBar.svelte
@@ -14,13 +14,13 @@
   
   <style>
     .taskbar {
-      position: absolute;
-      bottom: 0;
+      position: fixed;
       left: 0;
+      bottom: 0;
       width: 100%;
+      height: 30px;
       background-color: #333;
       color: white;
-      padding: 5px;
     }
   </style>
   
