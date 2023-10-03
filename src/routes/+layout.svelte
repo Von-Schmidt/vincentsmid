668d4a53d0faaf87c37ed4fb94f42c7ff4ea1403
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-10-03 22:57:56 +0200

    layout fix
    Fixed the main having padding and margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 	<main>
 		<slot />
+		
 	</main>
 </div>
 
@@ -20,24 +21,8 @@
         flex: 1;
         display: flex;
         flex-direction: column;
-        padding: 1rem;
-        width: 100vw/2;
-        margin: 1rem;
         box-sizing: border-box;
     }
-
-    footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-		text-align: center;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
 
 	@media (min-width: 480px) {
 		footer {
