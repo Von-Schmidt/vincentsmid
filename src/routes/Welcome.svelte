3adf00eeac5620dc09bc9d90700b1595b547d180
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-10-20 13:27:24 +0200

    Window body Welcome

---
+++ src/routes/Welcome.svelte
@@ -0,0 +1,9 @@
+<script>
+    import './styles.css';
+
+</script>
+
+<div>
+    <h1>Welcome to my website!</h1>
+    <p>My name is Vincent and I am a Software Developer. This website is currently under construction.</p>
+</div>
