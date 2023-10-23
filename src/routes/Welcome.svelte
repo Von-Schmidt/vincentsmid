3e3ee7da92d343c5651dc3b5e99ba1108fb1f975
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-10-23 19:23:31 +0200

    🤑 Welcome update

--- src/routes/Welcome.svelte
+++ src/routes/Welcome.svelte
@@ -7,10 +7,3 @@
     <h1>Welcome to my website!</h1>
     <p>My name is Vincent and I am a Software Developer. This website is currently under construction.</p>
 </div>
-
-<style>
-    div {
-        margin: 0;
-        padding: 0;
-    }
-</style>
