a14f982c679238f15e80a29ebfc256acfe51dac9
vincentsmid <vincent.smid@seznam.cz>
2024-01-06 22:33:44 +0100

    💄 Update welcome.svelte

--- src/routes/Welcome.svelte
+++ src/routes/Welcome.svelte
@@ -9,16 +9,41 @@
       DotLottiePlayer = module.default;
     }
   });
+
+  let showGreeting = false;
+  let playerVisible = true;
+  function handleAnimationComplete() {
+    playerVisible = false;
+    showGreeting = true;
+  }
 </script>
 
+{#if playerVisible}
+  <div class="player-container">
+    <dotlottie-player 
+        on:complete={handleAnimationComplete}
+        src="https://lottie.host/fd36e9cc-2cd6-4956-8751-2549d5abe64f/PsTImqB3O2.json"
+        background="transparent" 
+        speed="1" 
+        style="width: 57rem; height: 32rem;" 
+        autoplay>
+    </dotlottie-player>
+  </div>
+{/if}
 
-<dotlottie-player 
-    src="https://lottie.host/fd36e9cc-2cd6-4956-8751-2549d5abe64f/PsTImqB3O2.json" 
-    background="transparent" 
-    speed="1" 
-    style="width: 573px; height: 300px;" 
-    loop 
-    autoplay>
-</dotlottie-player>
+{#if showGreeting}
+  <div class="greeting">
+    <h1>Hi there!</h1>
+  </div>
+{/if}
+
+<style>
+  .player-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
 
 <!-- TODO: fix window fullscreen content scaling, add something after this animation, fix draggable bug-->
