48ea7056cb8bd9744588a61ad04d70533fa4bca0
vincentsmid <vincent.smid@seznam.cz>
2024-01-07 12:05:59 +0100

    🧑‍💻 Fix window styling with color variables

--- src/routes/WindowManager.svelte
+++ src/routes/WindowManager.svelte
@@ -2,7 +2,7 @@
     import Draggable from './Draggable.svelte';
     import {windows} from './store.js';
     import {fly} from 'svelte/transition';
-
+    
     function closeWindow(id) {
         windows.update(n => n.filter(win => win.id !== id));
     }
@@ -38,13 +38,13 @@
                     <div class="title-bar container">
                         <button class="svg-box container">
                             <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                                <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                                <circle cx="2.5" cy="2.5" r="2.5" />
                             </svg>
                             <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                                <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                                <circle cx="2.5" cy="2.5" r="2.5" />
                             </svg>
                             <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                                <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                                <circle cx="2.5" cy="2.5" r="2.5" />
                             </svg>
                         </button>
                         <span>{content.title}</span>
@@ -59,13 +59,13 @@
                 <div class="title-bar container">
                     <button class="svg-box container">
                         <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                            <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                            <circle cx="2.5" cy="2.5" r="2.5" />
                         </svg>
                         <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                            <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                            <circle cx="2.5" cy="2.5" r="2.5" />
                         </svg>
                         <svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" fill="none">
-                            <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
+                            <circle cx="2.5" cy="2.5" r="2.5" />
                         </svg>
                     </button>
                     <span>{content.title}</span>
@@ -84,83 +84,88 @@
 {/each}
 
 <style>
-    .window {
-        position: relative;
-        background-color: white;
-        border-radius: 0.8rem;
-        box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
-    }
+  @import 'styles.css';
+  .window {
+      position: relative;
+      background-color: var(--color-theme-2);
+      border-radius: 0.8rem;
+      box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
+  }
 
-    .title-bar {
-        background-color: #DFDFDF;
-        height: 3rem;
-        border-top-left-radius: 0.8rem;
-        border-top-right-radius: 0.8rem;
-        user-select: none;
-        cursor: grab;
-        color: #676767;
-        text-align: center;
-        font-size: 1.3rem;
-        font-weight: 600;
-        line-height: normal;
-        justify-content: space-between;
-    }
+  .title-bar {
+      background-color: var(--color-theme-1);
+      height: 3rem;
+      border-top-left-radius: 0.8rem;
+      border-top-right-radius: 0.8rem;
+      user-select: none;
+      cursor: grab;
+      color: var(--color-theme-3);
+      text-align: center;
+      font-size: 1.3rem;
+      font-weight: 600;
+      line-height: normal;
+      justify-content: space-between;
+  }
 
-    .container {
-        display: flex;
-        align-items: center;
-    }
+  .container {
+      display: flex;
+      align-items: center;
+  }
 
-    .button-box {
-        width: 6.4rem;
-        height: 2.2rem;
-        border-radius: 0.8rem;
-        background: #D4D4D4;
-        box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
-        margin-left: 0.8rem;
-        margin-right: 0.4rem;
-        justify-content: space-around;
-    }
+  .button-box {
+      width: 6.4rem;
+      height: 2.2rem;
+      border-radius: 0.8rem;
+      background: var(--color-theme-4);
+      box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
+      margin-left: 0.8rem;
+      margin-right: 0.4rem;
+      justify-content: space-around;
+  }
 
-    .button-close {
-        display: inline-block;
-        width: 1.6rem;
-        height: 1.6rem;
-        background-color: white;
-        border: none;
-        border-radius: 0.8rem;
-        box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
-        margin-top: 0.1rem;
-    }
+  .button-close {
+      display: inline-block;
+      width: 1.6rem;
+      height: 1.6rem;
+      background-color: var(--color-theme-5);
+      border: none;
+      border-radius: 0.8rem;
+      box-shadow: 0 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
+      margin-top: 0.1rem;
+  }
 
-    .button-close:hover {
-        background-color: #ff8787;
-        transition: 0.5s;
-    }
+  .button-close:hover {
+      background-color: var(--color-theme-8);
+      transition: 0.5s;
+  }
 
-    .svg-box {
-        background-color: transparent;
-        border: none;
-        color: transparent;
-        justify-content: space-between;
-        width: 2.6rem;
-        margin-left: 1rem;
-    }
+  .svg-box {
+      background-color: transparent;
+      border: none;
+      color: transparent;
+      justify-content: space-between;
+      width: 2.6rem;
+      margin-left: 1rem;
+  }
 
-    .window-content {
-        padding: 1rem;
-        margin: 0;
-    }
+  .svg-box svg {
+      fill: var(--color-theme-5);
+  }
 
-    .window.maximized {
-        width: 100vw !important;
-        height: 100vh !important;
-        border-radius: 0;
-        box-shadow: none;
-        position: fixed !important;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 11;
-    }
+  .window-content {
+      padding: 1rem;
+      margin: 0;
+  }
+
+  .window.maximized {
+      width: 100vw !important;
+      height: 100vh !important;
+      border-radius: 0;
+      box-shadow: none;
+      position: fixed !important;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 11;
+  }
 </style>
